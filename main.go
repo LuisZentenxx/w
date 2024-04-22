1e59966c67896ede8cb7ae27f2fabadafbd5c30e
Leo Araya <101936353+leoarayav@users.noreply.github.com>
2024-04-22 17:51:12 -0400

    feat(w): first application deploy

---
+++ main.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+// This is the main package of the application
+//
+// Usage: w --help | --version | [country]
+//
+// Options:
+//
+//	--help     Show this screen.
+//	--version  Show version.
+//
+// Arguments:
+//
+//	country    Country name where to get the weather information.
+func main() {
+	fmt.Println("W - Meterologic information for a given country and more")
+}
