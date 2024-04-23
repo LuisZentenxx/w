1e80e72dc4b24e2414abf5e67b94414821b97808
Fernan_dr0id <luis.zenteno02@inacapmail.cl>
2024-04-22 22:29:50 -0400

    [feat] Add extra types in Current

--- go.mod
+++ go.mod
@@ -2,4 +2,9 @@ module leoarayav/w
 
 go 1.22.2
 
-require github.com/joho/godotenv v1.5.1
+require (
+	github.com/joho/godotenv v1.5.1
+	
+)
+
+require golang.org/x/sys v0.0.0-20220715151400-c0bba94af5f8 // indirect
--- internal/types/types.go
+++ internal/types/types.go
@@ -36,5 +36,35 @@ type Current struct {
 	// The temperature of the location (in Celsius obviously)
 	Temperature float64 `json:"temperature"`
 
+	// The wind direction of the location (North, South, West, East)
+	WindDir string `json:"wind_dir"`
+
+	// The wind speed of the location (Km/h)
+	WindSpeed float64 `json:"wind_speed"`
+
+	//The wind degree
+	WindDegree string `json:"wind_degree"`
+
+	// Percent of humidity present in the air
+	Humidity float64 `json:"humidity"`
+
+	// The precipitation level (millimeters)
+	Precip float64 `json:"precip"`
+
+	// Ultraviolet index
+	UvIndex int `json:"uv_index"`
+
+	// The air pressure (millibar)
+	Pressure float64 `json:"pressure"`
+
+	// The cloud cover level in percentage
+	Cloudcover float64 `json:"cloudcover"`
+
+	// The "Feels Like" temperature
+	FeelsLike float64 `json:"feelslike"`
+
+	// The visibility level (kilometers)
+	Visibility float64 `json:"visibility"`
+
 	// TODO: Add more fields respected to the current weather showed in the REQUEST
 }
