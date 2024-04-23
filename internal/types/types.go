f7c9309d4387e6eae2eb83f02b9675f6ddad2f9c
Leo Araya <101936353+leoarayav@users.noreply.github.com>
2024-04-22 22:01:50 -0400

    feat(internal/types): some types requested

--- internal/types/types.go
+++ internal/types/types.go
@@ -1,4 +1,40 @@
 // This module contains the types used in the internal core of W
 package types
 
-// TODO: Add types below
+type Weather struct {
+	// The location of the weather
+	Location Location `json:"location"`
+
+	// The current weather
+	Current Current `json:"current"`
+}
+
+type Location struct {
+	// The name of the location
+	Name string `json:"name"`
+
+	// Country of the location
+	Country string `json:"country"`
+
+	// Region of the location
+	Region string `json:"region"`
+
+	// Latitude of the location
+	Lat float64 `json:"lat"`
+
+	// Longitude of the location
+	Lon float64 `json:"lon"`
+
+	// Timezone of the location
+	Timezone string `json:"timezone_id"`
+
+	// Local time of the location
+	Localtime string `json:"localtime"`
+}
+
+type Current struct {
+	// The temperature of the location (in Celsius obviously)
+	Temperature float64 `json:"temperature"`
+
+	// TODO: Add more fields respected to the current weather showed in the REQUEST
+}
